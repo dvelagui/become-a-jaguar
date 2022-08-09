9e0dcb5d22ea2a99ade0948d3d78754f86028ef4
dvelagui <dvelagui@gmail.com>
2022-08-08 20:08:32 -0500

    Ajustes 2da Revision

---
+++ docs/main.js
@@ -0,0 +1,11 @@
+
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  draggable: true,/* 
+  dots: '.dots', */
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
